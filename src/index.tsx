18e6651c87a3306f391cbb723110a892d8c5f144
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-01-24 17:30:10 +0100

    Some animations changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { render } from "react-dom";
 import styled from "@emotion/styled";
 
-import "./styles.scss";
 import { Love } from "./Love";
 import { Hate } from "./Hate";
 import { Happy } from "./Happy";
